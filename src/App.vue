1297a403ec86e4eeb397616a4c5ca30a8f943135
six-u <648977787@qq.com>
2022-05-02 09:51:01 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <router-view></router-view>
+  <div class="container">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
+<style scoped>
+/* .container {
+  width: 1366px;
+  height: 768px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+} */
+</style>
